7b91d1f41919cf7e829f2b9767fb0698d3408fce
romanpastu <roman.pastushenko.1997@gmail.com>
2019-03-27 13:17:31 +0100

    update

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipes.model';
+import { EventEmitter } from 'protractor';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -11,8 +12,9 @@ export class RecipeListComponent implements OnInit {
     'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'),
     new Recipe('A Test Recipe','This is a test',
     'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg')
-    
   ];
+
+  
 
   constructor() { }
 
